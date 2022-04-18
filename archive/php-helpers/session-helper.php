f1759844f4dc1bca41545c776b1ba32eb12da642
root <root@localhost.localdomain>
2022-04-18 01:10:02 +0000

    2

--- archive/php-helpers/session-helper.php
+++ archive/php-helpers/session-helper.php
@@ -6,47 +6,48 @@ $redis->connect('127.0.0.1', 6379);
 
 $temp = array();
 
-$login_time = 30;
+$login_time = 30*60;
 
 while ( $input = readline() ) {
-    $current_time = time();
+	$current_time = new DateTime("now");
+	
+	$temp = explode(' ', $input);
+	$answer = "ERR";
+	if (isset($temp[1])) {
+		$ip_login_time = $redis->get($temp[1]);
+		if ($ip_login_time !== false) {
+			$ip_login_time = new DateTime('@' . $ip_login_time);
+		
+			$ip_login_time = intval($ip_login_time->format('U'));
+			$diff = $current_time->format('U') - $ip_login_time;
+			if ( $diff > $login_time ) {
+				$answer = "ERR";
+			} else {
+				$answer = "OK";
+			}
+		} else {
+			$answer = "ERR";
+		}
 
-    $temp = explode(' ', $input);
-    $answer = "ERR";
-    if (isset($temp[1])) {
-	    $ip_login_time = $redis->get($temp[1]);
-	    if ($ip_login_time !== false) {
-		    $ip_login_time = intval($ip_login_time);
-		    if (($ip_login_time - time()) > (60 * $login_time) ) {
-			    $answer = "ERR";
-		    } else {
-			    $diff = ( ($ip_login_time - time()) > (60 * $login_time));
-			    $answer = "OK";
-
-		    }
-	    } else {
-	       $answer = "ERR";
-	    }
-
-    }
-
-    if (isset($temp[2])) {
-	    switch ($temp[2]) {
-	    case "LOGIN":
-		$ip_login_time = $redis->set($temp[1], time());
-		$answer = "OK message=\"Welcome\"";
-	        break;
-	    case "LOGOUT":
-		$redis->delete($temp[1]);
-		$answer = "OK message=\"ByeBye\"";
-	        break;
-	    case "QUIT":
-		    exit;
-	        break;
-	    }
-    }
-    $output = $temp[0] . " " . $answer;
-
-    echo $output."\n";
+	}
+	
+	if (isset($temp[2])) {
+		switch ($temp[2]) {
+		case "LOGIN":
+			$now = new DateTime("now");
+			$ip_login_time = $redis->set($temp[1], $now->format('U'));
+			$answer = "OK message=\"Welcome\"";
+	        	break;
+		case "LOGOUT":
+			$redis->delete($temp[1]);
+			$answer = "OK message=\"ByeBye\"";
+		        break;
+		case "QUIT":
+			exit;
+			break;
+		}
+	}
+	$output = $temp[0] . " " . $answer;
+	echo $output."\n";
 }
 ?>
